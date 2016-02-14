4b638247c2b3d6d3dd5a0fbd8058a6ec0f6ee86c
Arvind Borhade <arvindborhade@hotmail.com>
2016-02-15 00:59:22 +0530

    Create cotroller.php

---
+++ cotroller.php
@@ -0,0 +1,32 @@
+<?php 
+
+public function autocomplete($filter_name) {
+		$json = array();
+
+		if (isset($filter_name)) 
+		{
+			
+			$this->load->model('billpayment/payment');
+			$biilist=$this->model_billpayment_payment->biilist($filter_name);
+			$results=$biilist;
+
+			foreach ($results as $result) {
+				$json[] = array(
+					'id'       		=> $result['id'],
+					'name'	 		=> $result['name']
+				);
+			}
+		}
+
+		$sort_order = array();
+
+		foreach ($json as $key => $value) {
+			$sort_order[$key] = $value['name'];
+		}
+
+		array_multisort($sort_order, SORT_ASC, $json);
+
+		$this->response->addHeader('Content-Type: application/json');
+		$this->response->setOutput(json_encode($json));
+	}
+	
